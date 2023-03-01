ce0f6577c203c4faacbfe4b69512551fe972fb75
Animalpark <gabrielplinboy@gmail.com>
2023-03-01 16:38:34 -0300

    JavaScript Part Two - Basic

---
+++ JavaScript_Level_One-Basic/PartThree/Part3.js
@@ -0,0 +1,4 @@
+var weightPounds = prompt("Please put your weight here (in pounds/lbs)");
+var weightKilo = weightPounds * 0.454;
+alert("Your weight in kilograms is: " + weightKilo);
+console.log("Conversion completed")
